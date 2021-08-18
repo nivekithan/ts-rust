e2580a2793b85d873138f3c8b88d5d0508b13f16
Nivekithan S <nivekithan363@gmail.com>
2021-08-18 14:57:48 +0530

    Add method get_ident_name in token

--- packages/lexer/src/token.rs
+++ packages/lexer/src/token.rs
@@ -48,3 +48,43 @@ pub enum LiteralKind {
 
     String { name: String },
 }
+
+impl Token {
+    pub fn get_ident_name(&self) -> Result<&String, String> {
+        match self {
+            Self::Ident { name } => return Ok(name),
+            tok => Err(format!(
+                "Expected current token to be ident but got {:?}",
+                tok
+            )),
+        }
+    }
+}
+
+#[cfg(test)]
+mod token_test {
+    use super::Token;
+
+    #[test]
+    fn test_get_ident_name() {
+        let ident_token = Token::Ident {
+            name: "name".to_string(),
+        };
+
+        let name = &"name".to_string();
+
+        let expected_output: Result<&String, String> = Ok(name);
+        let actual_output = ident_token.get_ident_name();
+
+        assert_eq!(expected_output, actual_output);
+
+        let another_token = Token::Ampersand;
+
+        let error = String::from("Expected current token to be ident but got Ampersand");
+
+        let expected_output: Result<&String, String> = Err(error);
+        let actual_output = another_token.get_ident_name();
+
+        assert_eq!(expected_output, actual_output);
+    }
+}
