460df8022ee7e232f58a5ceb4b98291335ce2f6c
Nivekithan S <nivekithan363@gmail.com>
2021-08-19 22:40:50 +0530

    Change the location of tests in parser

--- packages/parser/src/lib.rs
+++ packages/parser/src/lib.rs
@@ -1,5 +1,8 @@
 pub mod parser;
 
+#[cfg(test)]
+mod tests;
+
 use crate::parser::Parser;
 use ast::Ast;
 use lexer::token::Token;
@@ -14,48 +17,4 @@ pub fn convert_to_ast(input: Vec<Token>) -> Vec<Ast> {
     }
 
     return asts;
-}
-
-#[cfg(test)]
-mod test {
-    use ast::{expression::Expression, Ast};
-    use lexer::convert_to_token;
-
-    use crate::convert_to_ast;
-
-    #[test]
-    fn test_convert_to_ast() {
-        let input = "
-        const x = 12;
-        const y = 23
-        const z =12;";
-
-        let expected_output = vec![
-            Ast::new_const_variable_declaration(
-                &"x".to_string(),
-                Expression::FloatLiteralExp {
-                    name: "12".to_string(),
-                    value: 12.0,
-                },
-            ),
-            Ast::new_const_variable_declaration(
-                &"y".to_string(),
-                Expression::FloatLiteralExp {
-                    name: "23".to_string(),
-                    value: 23.0,
-                },
-            ),
-            Ast::new_const_variable_declaration(
-                &"z".to_string(),
-                Expression::FloatLiteralExp {
-                    name: "12".to_string(),
-                    value: 12.0,
-                },
-            ),
-        ];
-
-        let actual_output = convert_to_ast(convert_to_token(input));
-
-        assert_eq!(actual_output, expected_output);
-    }
 }
---
+++ packages/parser/src/tests/mod.rs
@@ -0,0 +1,43 @@
+
+mod util;
+
+use ast::{expression::Expression, Ast};
+use lexer::convert_to_token;
+use crate::convert_to_ast;
+
+#[test]
+fn test_convert_to_ast() {
+    let input = "
+        const x = 12;
+        const y = 23
+        const z =12;";
+
+    let expected_output = vec![
+        Ast::new_const_variable_declaration(
+            &"x".to_string(),
+            Expression::FloatLiteralExp {
+                name: "12".to_string(),
+                value: 12.0,
+            },
+        ),
+        Ast::new_const_variable_declaration(
+            &"y".to_string(),
+            Expression::FloatLiteralExp {
+                name: "23".to_string(),
+                value: 23.0,
+            },
+        ),
+        Ast::new_const_variable_declaration(
+            &"z".to_string(),
+            Expression::FloatLiteralExp {
+                name: "12".to_string(),
+                value: 12.0,
+            },
+        ),
+    ];
+
+
+    let actual_output = convert_to_ast(convert_to_token(input));
+
+    assert_eq!(actual_output, expected_output);
+}
