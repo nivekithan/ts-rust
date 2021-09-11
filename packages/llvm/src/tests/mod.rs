c4e0efcad901f26e74aa5d64a6260ae27f3e7b42
Nivekithan S <nivekithan363@gmail.com>
2021-09-11 15:28:41 +0530

    Added new way to test langugae

--- packages/llvm/src/tests/mod.rs
+++ packages/llvm/src/tests/mod.rs
@@ -1,2 +1,1 @@
-pub(crate) mod utils;
 mod variable_declaration;
--- packages/llvm/src/tests/variable_declaration/const_variable_declaration.rs
+++ packages/llvm/src/tests/variable_declaration/const_variable_declaration.rs
@@ -1,11 +1,10 @@
 use ast::data_type::DataType;
 use lexer::convert_to_token;
 use parser::convert_to_ast;
+use test_utils::{DatatypeOrFn, ExpressionTest};
 
-use crate::{
-    tests::utils::{DatatypeOrFn, ExpressionTest},
-    write_llvm_ir,
-};
+use crate::write_llvm_ir;
+
 
 
 
@@ -38,6 +37,60 @@ fn test_all_const() {
 }
 
 #[test]
+fn explicit_float_type_const() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+        
+            let ast_input = "\n".to_string() +  ast_strings.join("\n").as_str();
+
+            let main_input = format!("const x : number = {}", main_strings[0]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![DatatypeOrFn::DataType(DataType::Float)],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test(vec!["_x".to_string()]);
+}
+
+
+
+#[test]
+fn explicit_boolean_type_const() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+        
+            let ast_input = "\n".to_string() +  ast_strings.join("\n").as_str();
+
+            let main_input = format!("const x : boolean = {}", main_strings[0]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![DatatypeOrFn::DataType(DataType::Boolean)],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test(vec!["_x".to_string()]);
+}
+
+#[test]
 fn test_const_unary_bang() {
     let exp_test = ExpressionTest {
         generate_input: Box::new(|ast_strings, main_strings| {
@@ -47,10 +100,182 @@ fn test_const_unary_bang() {
 
             let input = ast_input + main_input.as_str();
 
+            println!("{}", input);
+
             return input;
         }),
 
         expressions_data_type: vec![DatatypeOrFn::DataType(DataType::Boolean)],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test(vec!["_x".to_string()]);
+}
+
+
+
+
+#[test]
+fn unary_plus() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+        
+            let ast_input = "\n".to_string() +  ast_strings.join("\n").as_str();
+
+            let main_input = format!("const x  = +({})", main_strings[0]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![DatatypeOrFn::DataType(DataType::Float)],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test(vec!["_x".to_string()]);
+}
+
+
+#[test]
+fn unary_minus() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+        
+            let ast_input = "\n".to_string() +  ast_strings.join("\n").as_str();
+
+            let main_input = format!("const x  = -({})", main_strings[0]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![DatatypeOrFn::DataType(DataType::Float)],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test(vec!["_x".to_string()]);
+}
+
+
+
+
+#[test]
+fn binary_float_plus() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+        
+            let ast_input = "\n".to_string() +  ast_strings.join("\n").as_str();
+
+            let main_input = format!("const x  = ({}) + ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![DatatypeOrFn::DataType(DataType::Float), DatatypeOrFn::DataType(DataType::Float)],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test(vec!["_x".to_string()]);
+}
+
+
+
+#[test]
+fn binary_float_minus() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+        
+            let ast_input = "\n".to_string() +  ast_strings.join("\n").as_str();
+
+            let main_input = format!("const x  = ({}) - ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![DatatypeOrFn::DataType(DataType::Float), DatatypeOrFn::DataType(DataType::Float)],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test(vec!["_x".to_string()]);
+}
+
+
+
+#[test]
+fn binary_float_star() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+        
+            let ast_input = "\n".to_string() +  ast_strings.join("\n").as_str();
+
+            let main_input = format!("const x  = ({}) * ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![DatatypeOrFn::DataType(DataType::Float), DatatypeOrFn::DataType(DataType::Float)],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test(vec!["_x".to_string()]);
+}
+
+
+
+
+#[test]
+fn binary_float_slash() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+        
+            let ast_input = "\n".to_string() +  ast_strings.join("\n").as_str();
+
+            let main_input = format!("const x  = ({}) / ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![DatatypeOrFn::DataType(DataType::Float), DatatypeOrFn::DataType(DataType::Float)],
 
         test: Box::new(|_, _, input| {
             let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
