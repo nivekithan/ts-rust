58faf1fabf4b08162c35e83178fb55db614f7a4c
Nivekithan S <nivekithan363@gmail.com>
2021-09-14 14:41:31 +0530

    Add test case for previous commit

--- packages/llvm/src/tests/variable_declaration/let_variable_declaration.rs
+++ packages/llvm/src/tests/variable_declaration/let_variable_declaration.rs
@@ -380,3 +380,212 @@ fn binary_strict_equality_float() {
 
     exp_test.test();
 }
+
+#[test]
+fn binary_strict_not_equal_bool() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+            let ast_input = "\n".to_string() + ast_strings.join("\n").as_str();
+
+            let main_input = format!("let x  = ({}) !== ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![
+            (
+                DatatypeOrFn::DataType(DataType::Boolean),
+                vec!["_x".to_string()],
+            ),
+            (
+                DatatypeOrFn::DataType(DataType::Boolean),
+                vec!["__y".to_string()],
+            ),
+        ],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test();
+}
+#[test]
+fn binary_strict_not_equal_float() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+            let ast_input = "\n".to_string() + ast_strings.join("\n").as_str();
+
+            let main_input = format!("let x  = ({}) !== ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["_x".to_string()],
+            ),
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["__y".to_string()],
+            ),
+        ],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test();
+}
+
+
+
+#[test]
+fn binary_less_than() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+            let ast_input = "\n".to_string() + ast_strings.join("\n").as_str();
+
+            let main_input = format!("let x  = ({}) < ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["_x".to_string()],
+            ),
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["__y".to_string()],
+            ),
+        ],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test();
+}
+
+
+#[test]
+fn binary_less_than_or_equal() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+            let ast_input = "\n".to_string() + ast_strings.join("\n").as_str();
+
+            let main_input = format!("let x  = ({}) <= ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["_x".to_string()],
+            ),
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["__y".to_string()],
+            ),
+        ],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test();
+}
+
+
+#[test]
+fn binary_greater_than() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+            let ast_input = "\n".to_string() + ast_strings.join("\n").as_str();
+
+            let main_input = format!("let x  = ({}) > ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["_x".to_string()],
+            ),
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["__y".to_string()],
+            ),
+        ],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test();
+}
+
+
+#[test]
+fn binary_greater_than_or_equal() {
+    let exp_test = ExpressionTest {
+        generate_input: Box::new(|ast_strings, main_strings| {
+            let ast_input = "\n".to_string() + ast_strings.join("\n").as_str();
+
+            let main_input = format!("let x  = ({}) >= ({})", main_strings[0], main_strings[1]);
+
+            let input = ast_input + main_input.as_str();
+
+            return input;
+        }),
+
+        expressions_data_type: vec![
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["_x".to_string()],
+            ),
+            (
+                DatatypeOrFn::DataType(DataType::Float),
+                vec!["__y".to_string()],
+            ),
+        ],
+
+        test: Box::new(|_, _, input| {
+            let output = write_llvm_ir(convert_to_ast(convert_to_token(input.as_str())));
+
+            insta::assert_snapshot!(input, output);
+        }),
+    };
+
+    exp_test.test();
+}
+
